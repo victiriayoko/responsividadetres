1dc5c2653827a783b3e131d80ba8cc9e3e1cd1e0
victoriayoko <132665912+victiriayoko@users.noreply.github.com>
2024-08-15 08:10:36 -0300

    Add files via upload

---
+++ responsividade-main/responsividade-main/responsividade-main/menu.js
@@ -0,0 +1,35 @@
+
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+    item.addEventListener('click',selectLink)
+)
+
+var btnExp = document.querySelector('#btn-exp')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+
+})
+
+var btnEx = document.querySelector('#btn-ex')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnEx.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+
+})
+
+
+
+
+
+
